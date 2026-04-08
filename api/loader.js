99bbba7f36cd73d106f37690ffce342f708372db
isnotsin <email@isnotsin.com>
2026-04-09 01:36:56 +0800

    revamp

--- api/loader.js
+++ api/loader.js
@@ -2,6 +2,19 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
+  const accept = req.headers["accept"] || "";
+
+  if (accept.includes("text/html")) {
+    res.setHeader("Content-Type", "text/html");
+    return res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head><title>404 - Not Found</title></head>
+        <body><h1>404</h1><p>Page not found.</p></body>
+      </html>
+    `);
+  }
+
   const script = fs.readFileSync(path.join(process.cwd(), "core"), "utf8");
   res.setHeader("Content-Type", "text/plain");
   res.send(script);
