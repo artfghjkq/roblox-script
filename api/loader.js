5f1d8b263e3bc6af592f79601d03185ee807c023
isnotsin <imnotsin18@gmail.com>
2026-04-09 02:28:56 +0800

    change loader

--- api/loader.js
+++ api/loader.js
@@ -2,17 +2,52 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const accept = req.headers["accept"] || "";
+  const acceptHeader = req.headers["accept"] || "";
 
-  if (accept.includes("text/html")) {
-    res.setHeader("Content-Type", "text/html");
-    return res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head><title>404 - Not Found</title></head>
-        <body><h1>404</h1><p>Page not found.</p></body>
-      </html>
-    `);
+  if (acceptHeader.includes("text/html")) {
+    const arceeAsciiArt = `
+⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠉⢁⣀⣀⣀⡈⠉⠛⢿⡿⠿⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠏⢀⣴⣾⣿⣿⣿⣿⣿⡟⠃⢀⣀⣤⣤⣄⠉⢿⣿
+⣿⣿⣿⣿⣿⡏⠀⣾⣿⣿⣿⣿⣿⣿⠏⠀⣴⣿⣿⣿⣯⣻⣧⠀⢻
+⣿⣿⣿⣿⣿⠁⢸⣿⣿⣿⣿⣿⣿⣿⠀⠸⣿⣿⣿⣿⣿⣿⣿⡇⠈
+⣿⣿⣿⣿⡏⠀⣼⣿⣿⣿⣿⣿⣿⣿⣧⠀⠹⢿⣿⣿⣿⡿⠟⠀⣼
+⣿⣿⣿⡿⠇⠀⠛⠿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⡈⠉⠀⠀⣴⣿⣿
+⣿⡿⠁⣀⢠⢤⣤⠀⠀⠉⢀⠀⠀⠈⠉⠻⢿⣿⣿⣿⡇⠀⣿⣿⣿
+⡟⠀⣴⣽⣷⣷⠆⠀⣴⣾⣿⣔⡳⢦⡄⣄⣠⣿⣿⣿⡇⠀⣿⣿⣿
+⠀⢰⣿⣿⣿⠇⠀⣼⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⠀⢻⣿⣿
+⠀⠸⣾⣿⣿⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⣿⣿
+⣧⠀⠻⢿⣿⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⣿⣿
+⣿⣷⣤⣀⣈⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⠟⠙⣿⣿⣿⡏⠀⣼⣿⣿
+⣿⣿⣿⣿⣿⡇⠀⣄⠀⠙⠛⠿⠿⠛⠁⢀⣼⣿⣿⣿⡇⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣷⡀⠘⠿⠶⠀⢀⣤⣤⡀⠙⢿⣿⣿⡿⠁⢰⣿⣿⣿
+⢻⣿⣿⣿⣿⣿⣿⣦⣤⣤⣴⣿⣿⣿⣷⣄⣀⠈⠁⣀⣠⣿⣿⣿⣿
+⣹⣿⣿⣿⡿⢋⣩⣬⣩⣿⠃⣿⣿⣿⣿⢸⣿⡿⢋⣡⣬⣩⣿⣿⣿
+⡗⣿⣿⣿⣧⣈⣛⠛⠻⣿⠀⣿⣿⣿⡿⢸⣿⣧⣈⣛⠛⠻⣿⣿⣿
+⣿⣿⣿⣿⠹⣿⣿⡿⠂⣿⣇⠸⣿⣿⠃⣼⣿⠻⣿⣿⡿⠀⣿⣿⣿
+⣿⣿⣿⣿⣶⣤⣤⣴⣾⣿⣿⣶⣤⣤⣾⣿⣿⣶⣤⣤⣴⣾⣿⣿⣿
+    `.trim();
+
+    const errorMessages = `
+404 - Route Not Found!
+Arcee detected an unmapped path. Destination inaccessible.
+We're on a mission, but this isn't it.
+    `.trim();
+
+    const credits = `
+@isnotsin | t.me/isnotsin
+    `.trim();
+
+    const fullTextResponse = `
+${errorMessages}
+
+${arceeAsciiArt}
+
+${credits}
+    `.trim();
+
+    res.setHeader("Content-Type", "text/plain");
+    res.statusCode = 404;
+    return res.send(fullTextResponse);
   }
 
   const script = fs.readFileSync(path.join(process.cwd(), "core"), "utf8");
